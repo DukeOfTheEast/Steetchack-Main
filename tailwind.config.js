b1651029bb715990ba20ae855c304ed6c431e85c
Ukachukwu Ebuka <fralexander1999@gmail.com>
2023-12-21 20:29:32 +0100

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        navDark: "rgba(43, 43, 41, 1)",
+      },
+    },
   },
   plugins: [],
 };
